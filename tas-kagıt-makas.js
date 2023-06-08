aa4159e941f92d2332f4a59a69c444f94115fe8a
burhan <alacaburhan122016@gmail.com>
2023-06-08 18:09:02 +0300

    choice

--- tas-kagıt-makas.js
+++ tas-kagıt-makas.js
@@ -3,15 +3,46 @@
 
 
 
+//!resimler
+const rockImg=document.getElementById("rock")
+const paperImg=document.getElementById("paper")
+const scissorImg=document.getElementById("scissor") 
+
+
+//! seçilen elemanların taşıyıcıları
+const yourChoiceDiv = document.getElementById("your-choice")
+
+
+
+
 
 // **---- Variables ---***/
+let image =document.createElement("img")
+
+
+
 
 
 
 // *** Event Listeners -----**/
 
+rockImg.addEventListener("click" , ()=>{
+    
+    image.src="./assets/rock.png"
+    image.alt="rock"
+    yourChoiceDiv.appendChild(image)
 
+// yourChoiceDiv.innerHTML=`<img src="./assets/rock.png" alt="rock">`
+    
+})
 
+paperImg.addEventListener("click" , ()=>{
+    
+    image.src="./assets/paper.png"
+    image.alt="paper"
+    yourChoiceDiv.appendChild(image)})
+
+yourChoiceDiv.innerHTML=`<img src="./assets/paper.png" alt="paper">`
 
 
 // ****--Functions
