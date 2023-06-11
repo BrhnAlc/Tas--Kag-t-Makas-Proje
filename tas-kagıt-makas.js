55d082b07fb51eb41e2a4b565f71d8e4206a5830
burhan <alacaburhan122016@gmail.com>
2023-06-11 11:33:40 +0300

    you lost

--- tas-kagıt-makas.js
+++ tas-kagıt-makas.js
@@ -1,48 +1,27 @@
 
-// *** ----- Selectors-----*/
-
-
-
-//!resimler
-const rockImg=document.getElementById("rock")
-const paperImg=document.getElementById("paper")
-const scissorImg=document.getElementById("scissor") 
-
-
-//! seçilen elemanların taşıyıcıları
-const yourChoiceDiv = document.getElementById("your-choice")
-
-
-
-
-
-// **---- Variables ---***/
-let image =document.createElement("img")
+//! SELECTORS 
 
 
 
 
 
 
-// *** Event Listeners -----**/
 
-rockImg.addEventListener("click" , ()=>{
-    
-    image.src="./assets/rock.png"
-    image.alt="rock"
-    yourChoiceDiv.appendChild(image)
-
-// yourChoiceDiv.innerHTML=`<img src="./assets/rock.png" alt="rock">`
-    
-})
-
-paperImg.addEventListener("click" , ()=>{
-    
-    image.src="./assets/paper.png"
-    image.alt="paper"
-    yourChoiceDiv.appendChild(image)})
-
-yourChoiceDiv.innerHTML=`<img src="./assets/paper.png" alt="paper">`
+//! VARİABLES
 
 
-// ****--Functions
+
+
+
+
+//!EVENT LİSTENERS
+
+
+
+
+
+
+
+
+
+//! FUNCTİON
