655b74d3d1f18af508750c40bb550d14b803934a
burhan <alacaburhan122016@gmail.com>
2023-06-11 12:19:17 +0300

    innerHTML

--- tas-kagıt-makas.js
+++ tas-kagıt-makas.js
@@ -4,6 +4,20 @@
 
 
 
+//?   Resimler
+const rockImg=document.querySelector(`#rock`);
+// const rockImg=document.querySelector(`#paper`);
+// const rockImg=document.querySelector(`#scisser`);
+
+const yourChoiceDiv=document.querySelector(`#your-choice`)
+
+
+rockImg.addEventListener(`click`,()=>{
+
+    yourChoiceDiv.innerHTML=`<img src="./assets/rock.png" alt="rock">`
+});
+
+
 
 
 
