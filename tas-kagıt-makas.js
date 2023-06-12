5b045d50063c6521e414f0d0ad56cc965bfaec2a
burhan <alacaburhan122016@gmail.com>
2023-06-13 00:46:37 +0300

    event

--- tas-kagıt-makas.js
+++ tas-kagıt-makas.js
@@ -1,75 +1,30 @@
+            //!SELECTORS
+  const rockImg=document.querySelector(`#rock`);
+  
+  const paperImg=document.querySelector(`#paper`);
 
-                //! SELECTORS 
+  const scissorImg=document.querySelector(`#scissor`);
 
-   const selectionArticle=document.querySelector(`.selection`);
+  const yourChoiceDiv=document.querySelector(`#your-choice`);
 
-                 
-           //! SEÇİLEN ELEMANLARIN TAYİNİCİSİ
-      const yourChoiceDiv=document.getElementById(`your-choice`);
-     const pcChoiceDiv=document.getElementById(`pc-choice`)
-                         //! VARİABLES
-            let userSelectImg=document.createElement("img");
-            let pcSlectImg=document.createElement(`img`)
-            //!EVENT
+  const pcChoice=document.querySelector(`#your-choice`);
+  
 
-selectionArticle.addEventListener(`click`, (e)=>{
-    console.log(e.target.id);
-
-if(e.target.id){
-    userSelectImg.src=`./assets/${e.target.id}.png`
-    userSelectImg.alt=e.target.id
-    yourChoiceDiv.appendChild(userSelectImg)
+          //! VARİABLES
 
 
-    createPcSelection()
-}});
+          //!EVENT
 
-const createPcSelection=()=>{
-const pcArr=["rock","paper","scissor"]
-const pcRandom=pcArr[Math.floor(Math.random()*3)]
+    rockImg.addEventListener(`click`,function(){
+        let image=document.createElement(`img`);
 
-pcSlectImg.src=`./assets/${pcRandom}.png`
-pcSlectImg.alt=pcRandom
-pcChoiceDiv.appendChild(pcSlectImg)
+     image.src="./assets/rock.png";
 
-}
+     image.alt="rock";
+
+    yourChoiceDiv.appendChild(image);
+     
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// //? RESİMLER
-
-// const rockImg=document.getElementById(`rock`);
-// const paperImg =document.getElementById(`paper`);
-// const scissorImg=document.getElementById(`scissor`);
-
-    //                     //!EVENT LİSTENERS
-    //   rockImg.addEventListener(`click`,()=>{
-
-    //     image.src="./assets/rock.png"
-    //     image.alt="rock"
-    //     yourChoiceDiv.appendChild(image)
-    //   })
-              
-    //      paperImg.addEventListener(`click`,()=>{
-    //         image.src="./assets/paper.png"
-    //         image.alt="paper"
-    //         yourChoiceDiv.appendChild(image)
-    //      })
-
-
-    //      scissorImg.addEventListener(`click`,()=>{
-    //         image.src="./assets/scissor.png"
-    //         image.alt="scissor"
-    //         yourChoiceDiv.appendChild(image)
-    //      })
+    })
